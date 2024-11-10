d723a5542c2acda334f6f0e5bc19d62a65581b0b
prinzeval <valentineallpowers@gmail.com>
2024-11-10 18:10:07 +0200

    hello

--- route.py
+++ route.py
@@ -36,9 +36,5 @@ async def scrape_movies(request: ScrapeRequest):
         "script_outputs": script_outputs
     }
 
-
 if __name__ == "__main__":
-    # Get the PORT from the Render environment variables
-    port = int(os.getenv("PORT", 8000))  # Fallback to 8000 if not set
-    
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=8070)
